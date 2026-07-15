5d7121407a91ba1c9cf5725c1d01cb03b3db6dde
혁준 <revo3325@gmail.com>
2026-07-15 20:03:57 +0900

    test

---
+++ src/main/java/com/mycom/myapp/MoiGoApplication.java
@@ -0,0 +1,12 @@
+package com.mycom.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoiGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoiGoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/mycom/myapp/MoiGoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.mycom.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoiGoApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
